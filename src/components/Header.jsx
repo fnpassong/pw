0755959986e4d922d4216c1bab8416f7f0087e6c
fnpassong <139728483+fnpassong@users.noreply.github.com>
2024-12-05 22:24:49 -0500

    fin <Header/>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,26 @@ const Header = () => {
         <img src={logo} alt="logo de proyecto Web" className="header__logo" />
         <p className="header__title ff-accent">Proyecto Web</p>
       </div>
+      <div className="hero">
+        <div className="hero__wrapper grid-layout">
+          <div className="hero__pic"></div>
+          <div className="hero__text-group">
+            <h1 className="hero__title ff-accent">
+              Creamos páginas webs personalizadas
+            </h1>
+            <div className="hero__paragraphs flow">
+              <p className="hero__note">
+                Impresiones duraderas en un mundo digital
+              </p>
+              <p className="hero__paragraph">
+                Construimos páginas web para dar a conocer a tu empresa,
+                aumentando su visibilidad y mostrando los productos y/o servicio
+                que ofreces
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
