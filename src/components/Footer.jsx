703aaccc757bd43e10defd952a592dc0d35bd267
fnpassong <139728483+fnpassong@users.noreply.github.com>
2024-12-23 19:05:29 -0500

    <Footer/>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,18 @@
 const Footer = () => {
-  return <h1>hi</h1>
+  return (
+    <section className="footer">
+      <div className="footer__wrapper">
+        <div className="footer__text">
+          <h3 className="footer__subtitle uppercase text-center ff-accent">
+            deseas contactarnos
+          </h3>
+          <p className="footer__paragraph text-center">
+            Escribenos a <code>contacto@proyectoweb.site</code>
+          </p>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Footer
